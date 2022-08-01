501eff11aabd074ab5265ef44bff6981cb70b6be
Christian <felix.christian.reyes@gmail.com>
2022-08-01 18:39:57 +0800

    added section

---
+++ src/pages/Home/Section.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Section = (props) => {
+  return (
+    <div className="section-title">
+      <h2>{props.text}</h2>
+    </div>
+  );
+};
+
+export default Section;
