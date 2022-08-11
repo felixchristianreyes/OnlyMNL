a72ae663652a140764c666adbcbf2388ec22b31e
Christian <felix.christian.reyes@gmail.com>
2022-08-11 22:56:33 +0800

    added textcenter class

--- src/pages/Home/Services/Card.js
+++ src/pages/Home/Services/Card.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Card = ({ title, icon, description }) => {
   return (
-    <div className="col-xl-3 col-md-6 d-flex align-items-stretch">
+    <div className="col-xl-3 col-md-6 d-flex align-items-stretch text-center">
       <div className="icon-box">
         <div className="icon text-center">
           <div className="cardIcon">{icon}</div>
