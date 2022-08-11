58e179bdd065eeb8f0baa0a531ed939c52e3fb09
Christian <felix.christian.reyes@gmail.com>
2022-08-11 22:52:48 +0800

    added card components

--- src/pages/Home/Services/Services.js
+++ src/pages/Home/Services/Services.js
@@ -1,12 +1,10 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import "./Services.css";
-import {
-  BsFillLightningChargeFill,
-  BsClock,
-  BsFillBadgeAdFill,
-  BsWatch,
-} from "react-icons/bs";
+import Card from "./Card";
+import { AiOutlineUser, AiOutlineDatabase } from "react-icons/ai";
+import { BiBuildingHouse } from "react-icons/bi";
+import { BsShopWindow } from "react-icons/bs";
 
 const Services = () => (
   <section id="services" className="services section-bg" data-testid="Services">
@@ -20,93 +18,38 @@ const Services = () => (
       </div>
 
       <div className="row">
-        <div
-          className="col-xl-3 col-md-6 d-flex align-items-stretch"
+        {/* User */}
+        <Card
+          title="User"
+          icon={<AiOutlineUser />}
+          description="You want to share your experience? You have some suggestions to enhance our application? You want to share your happiness or your disappointment? You have any question or want some extra information?"
           data-aos="zoom-in"
           data-aos-delay="100"
-        >
-          <div className="icon-box">
-            <div className="icon d-flex">
-              <h4>
-                Fast{" "}
-                <span>
-                  <BsFillLightningChargeFill />
-                </span>
-              </h4>
-            </div>
-
-            <p>
-              Our website is so fast that you'd be able to get navigate your way
-              to your preferred destination at a nick of a time
-            </p>
-          </div>
-        </div>
-
-        <div
-          className="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-md-0"
+        />
+        {/* City Representative */}
+        <Card
+          title="City representative"
+          icon={<BiBuildingHouse />}
+          description="You have public toilets on your premises and you’d like them to be included in our database? You have paper or digital documents you’d like to share with us and appear as an official city contributor in our application?"
           data-aos="zoom-in"
-          data-aos-delay="200"
-        >
-          <div className="icon-box">
-            <div className="icon d-flex">
-              <h4>
-                Availability{" "}
-                <span>
-                  <BsClock />
-                </span>
-              </h4>
-            </div>
-
-            <p>
-              You can guarantee that the server is running 24/7 and the website
-              would be of service to you at any given time
-            </p>
-          </div>
-        </div>
-
-        <div
-          className="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0"
+          data-aos-delay="100"
+        />
+        {/* Brand representative */}
+        <Card
+          title="Brand representative"
+          icon={<BsShopWindow />}
+          description="You want to promote your brand through our application? You want some visibility and, for instance, want to display your retail shops in our app?"
           data-aos="zoom-in"
-          data-aos-delay="300"
-        >
-          <div className="icon-box">
-            <div className="icon d-flex">
-              <h4>
-                No Ads{" "}
-                <span>
-                  <BsFillBadgeAdFill />
-                </span>
-              </h4>
-            </div>
-
-            <p>
-              Who would want ads when you are going through 5 stages of grief?
-              That's why no ads. F.O.R.E.V.E.R
-            </p>
-          </div>
-        </div>
-
-        <div
-          className="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0"
+          data-aos-delay="100"
+        />
+        {/* You want to use our database */}
+        <Card
+          title="Database"
+          icon={<AiOutlineDatabase />}
+          description="You need our database for some specific purpose on your company or api?"
           data-aos="zoom-in"
-          data-aos-delay="400"
-        >
-          <div className="icon-box">
-            <div className="icon d-flex">
-              <h4>
-                Direct{" "}
-                <span>
-                  <BsWatch />
-                </span>
-              </h4>
-            </div>
-
-            <p>
-              No beating around the bush. Get to your{" "}
-              <a href="locator">Toilet Seat</a> now!
-            </p>
-          </div>
-        </div>
+          data-aos-delay="100"
+        />
       </div>
     </div>
   </section>
